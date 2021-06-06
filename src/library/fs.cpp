c0a216295fd758217463570eadaa6b3312589466
Nole <universe_fly@icloud.com>
2021-06-07 02:45:25 +0800

    Implement stat and maybe implement remove

--- src/library/fs.cpp
+++ src/library/fs.cpp
@@ -14,66 +14,66 @@
 // Debug file system -----------------------------------------------------------
 
 void FileSystem::debug(Disk *disk) {
-    Block block;
+  Block block;
 
-    // Read Superblock
-    disk->read(0, block.Data);
+  // Read Superblock
+  disk->read(0, block.Data);
 
-    printf("SuperBlock:\n");
-    printf("    magic number is %s\n", block.Super.MagicNumber == MAGIC_NUMBER ? "valid" : "invalid");
-    printf("    %u blocks\n"         , block.Super.Blocks);
-    printf("    %u inode blocks\n"   , block.Super.InodeBlocks);
-    printf("    %u inodes\n"         , block.Super.Inodes);
+  printf("SuperBlock:\n");
+  printf("    magic number is %s\n", block.Super.MagicNumber == MAGIC_NUMBER ? "valid" : "invalid");
+  printf("    %u blocks\n"         , block.Super.Blocks);
+  printf("    %u inode blocks\n"   , block.Super.InodeBlocks);
+  printf("    %u inodes\n"         , block.Super.Inodes);
 
-    // The total number of Inode blocks
-    const uint32_t inodeBlocks = block.Super.InodeBlocks;
-    const uint32_t inodeCount = block.Super.Inodes;
-    for (uint32_t i = 0; i != inodeBlocks; ++i) {
-      // +1 cuz inode blocks start from 1
-      disk->read(i + 1, block.Data);
-      for (uint32_t inodeIndex = 0; inodeIndex != INODES_PER_BLOCK; ++inodeIndex) {
-        // overall index over all inodes
-        const auto inodeOverallIndex = i * INODES_PER_BLOCK + inodeIndex;
-        // There wouldn't be any valid inodes
-        if (i == inodeBlocks - 1 && inodeOverallIndex >= inodeCount) {
-          break;
-        }
-        const auto &inode = block.Inodes[inodeIndex];
-        if (inode.Valid == 1) {
-          printf("Inode %u:\n", inodeOverallIndex);
-          printf("    size: %u bytes\n", inode.Size);
-          // The total number of blocks related to this inode
-          // x + y - 1 / y == ceil(x/y)
-          const uint32_t totalBlocks = (inode.Size + Disk::BLOCK_SIZE - 1) / Disk::BLOCK_SIZE;
-          // Here we only calculate the direct blocks. 5 here cuz for an inode block 5 ptrs are direct.
-          if (totalBlocks <= 5) {
-            // only direct blocks
-            printf("    direct blocks:");
-            for (uint32_t k = 0; k != totalBlocks; ++k) {
-              printf(" %u", inode.Direct[k]);   
-            }
-            printf("\n");
-          } else {
-            // first print 5 direct blocks
-            printf("    direct blocks: %u %u %u %u %u\n",
-                   inode.Direct[0], inode.Direct[1], inode.Direct[2],
-                   inode.Direct[3], inode.Direct[4]);
-            // then print the indirect block
-            printf("    indirect block: %u\n", inode.Indirect);
-            // finally print all indirect blocks in this indirect block
-            // k stands for the indirect block index, starting from 5
-            // k + 5 != ... instead of k != ... - 5 cuz they're unsigned
-            Block indirectBlock;
-            disk->read(inode.Indirect, indirectBlock.Data);
-            printf("    indirect data blocks:");
-            for (uint32_t k = 0; k + 5 != totalBlocks; ++k) {
-              printf(" %u", indirectBlock.Pointers[k]);
-            }
-            printf("\n");
+  // The total number of Inode blocks
+  const uint32_t inodeBlocks = block.Super.InodeBlocks;
+  const uint32_t inodeCount = block.Super.Inodes;
+  for (uint32_t i = 0; i != inodeBlocks; ++i) {
+    // +1 cuz inode blocks start from 1
+    disk->read(i + 1, block.Data);
+    for (uint32_t inodeIndex = 0; inodeIndex != INODES_PER_BLOCK; ++inodeIndex) {
+      // overall index over all inodes
+      const auto inodeOverallIndex = i * INODES_PER_BLOCK + inodeIndex;
+      // There wouldn't be any valid inodes
+      if (i == inodeBlocks - 1 && inodeOverallIndex >= inodeCount) {
+        break;
+      }
+      const auto &inode = block.Inodes[inodeIndex];
+      if (inode.Valid == 1) {
+        printf("Inode %u:\n", inodeOverallIndex);
+        printf("    size: %u bytes\n", inode.Size);
+        // The total number of blocks related to this inode
+        // x + y - 1 / y == ceil(x/y)
+        const uint32_t totalBlocks = (inode.Size + Disk::BLOCK_SIZE - 1) / Disk::BLOCK_SIZE;
+        // Here we only calculate the direct blocks. 5 here cuz for an inode block 5 ptrs are direct.
+        if (totalBlocks <= 5) {
+          // only direct blocks
+          printf("    direct blocks:");
+          for (uint32_t k = 0; k != totalBlocks; ++k) {
+            printf(" %u", inode.Direct[k]);   
           }
+          printf("\n");
+        } else {
+          // first print 5 direct blocks
+          printf("    direct blocks: %u %u %u %u %u\n",
+                 inode.Direct[0], inode.Direct[1], inode.Direct[2],
+                 inode.Direct[3], inode.Direct[4]);
+          // then print the indirect block
+          printf("    indirect block: %u\n", inode.Indirect);
+          // finally print all indirect blocks in this indirect block
+          // k stands for the indirect block index, starting from 5
+          // k + 5 != ... instead of k != ... - 5 cuz they're unsigned
+          Block indirectBlock;
+          disk->read(inode.Indirect, indirectBlock.Data);
+          printf("    indirect data blocks:");
+          for (uint32_t k = 0; k + 5 != totalBlocks; ++k) {
+            printf(" %u", indirectBlock.Pointers[k]);
+          }
+          printf("\n");
         }
       }
     }
+  }
 }
 
 // Format file system ----------------------------------------------------------
@@ -169,7 +169,6 @@ void FileSystem::initFreeBlocks_forInodeBlock(const Inode (&inodes)[INODES_PER_B
         // k + 5 != ... instead of k != ... - 5 cuz they're unsigned
         Block indirectBlock;
         disk->read(inode.Indirect, indirectBlock.Data);
-        printf("    indirect data blocks:");
         for (uint32_t k = 0; k + 5 != totalBlocks; ++k) {
           freeBlocks[indirectBlock.Pointers[k]] = false;
         }
@@ -210,39 +209,83 @@ ssize_t FileSystem::create() {
 // Remove inode ----------------------------------------------------------------
 
 bool FileSystem::remove(size_t inumber) {
-    // Load inode information
+  // Load inode information
+  Block inodeBlock;
+  uint32_t inodeBlkIndex = inumber / INODES_PER_BLOCK + 1;
+  uint32_t offset = inumber % INODES_PER_BLOCK;
+  disk->read(inodeBlkIndex, inodeBlock.Data);
+  auto &inode = inodeBlock.Inodes[offset];
+  if (inode.Valid == 0) { return false; }
 
-    // Free direct blocks
+  // The total number of blocks related to this inode
+  // x + y - 1 / y == ceil(x/y)
+  const uint32_t totalBlocks = (inode.Size + Disk::BLOCK_SIZE - 1) / Disk::BLOCK_SIZE;
+  // Here we only calculate the direct blocks. 5 here cuz for an inode block 5 ptrs are direct.
+  if (totalBlocks == 0) {}
+  else if (totalBlocks <= 5) {
+    // free direct blocks
+    for (uint32_t k = 0; k != totalBlocks; ++k) {
+      freeBlocks[inode.Direct[k]] = true;
+    }
+  } else {
+    // free indirect blocks
+    freeBlocks[inode.Direct[0]] = true;
+    freeBlocks[inode.Direct[1]] = true;
+    freeBlocks[inode.Direct[2]] = true;
+    freeBlocks[inode.Direct[3]] = true;
+    freeBlocks[inode.Direct[4]] = true;
+    freeBlocks[inode.Indirect] = true;
 
-    // Free indirect blocks
+    // k stands for the indirect block index, starting from 5
+    // k + 5 != ... instead of k != ... - 5 cuz they're unsigned
+    Block indirectBlock;
+    disk->read(inode.Indirect, indirectBlock.Data);
+    for (uint32_t k = 0; k + 5 != totalBlocks; ++k) {
+      freeBlocks[indirectBlock.Pointers[k]] = true;
+    }
+  }
 
-    // Clear inode in inode table
-    return true;
+  // Clear inode in inode table
+  // No need to clean other fields since it's an invalid inode
+  inode.Valid = 0;
+  disk->write(inodeBlkIndex, inodeBlock.Data);
+
+  return true;
 }
 
 // Inode stat ------------------------------------------------------------------
 
 ssize_t FileSystem::stat(size_t inumber) {
-    // Load inode information
-    return 0;
+  // Load inode information
+  Block inodeBlock;
+  uint32_t inodeBlkIndex = inumber / INODES_PER_BLOCK + 1;
+  uint32_t offset = inumber % INODES_PER_BLOCK;
+  
+  if (inodeBlkIndex > disk->size()) { return -1; }
+  disk->read(inodeBlkIndex, inodeBlock.Data);
+  if (inodeBlock.Inodes[offset].Valid == 1) {
+    return inodeBlock.Inodes[offset].Size;
+  }
+
+  return -1;
 }
 
 // Read from inode -------------------------------------------------------------
 
 ssize_t FileSystem::read(size_t inumber, char *data, size_t length, size_t offset) {
-    // Load inode information
+  // Load inode information
 
-    // Adjust length
+  // Adjust length
 
-    // Read block and copy to data
-    return 0;
+  // Read block and copy to data
+  return 0;
 }
 
 // Write to inode --------------------------------------------------------------
 
 ssize_t FileSystem::write(size_t inumber, char *data, size_t length, size_t offset) {
-    // Load inode
+  // Load inode
     
-    // Write block and copy to data
-    return 0;
+  // Write block and copy to data
+  return 0;
 }
